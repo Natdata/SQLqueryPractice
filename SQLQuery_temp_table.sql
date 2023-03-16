01928e6af58021f776e984a85d5e712da4a61b11
Natalia Pijanowska <125644275+Natdata@users.noreply.github.com>
2023-03-16 21:10:52 +0100

    Temp_table_practice

---
+++ SQLQuery_temp_table.sql
@@ -0,0 +1,28 @@
+-- Create Temp Table 
+
+SELECT vendor_id,
+Product_id,
+sum(quantity * original_price) as WholePayment
+FROM farmers_market.vendor_inventory
+group by 1,2;
+
+ 
+IF (SELECT object_id('TempDB..#Temp_WholePayment ')) IS NOT NULL
+BEGIN
+    DROP TABLE #Temp_WholePayment 
+END
+Create Table #Temp_WholePayment 
+(
+VendorID int,
+ProductID int,
+WholePayment float)
+
+Insert Into #Temp_WholePayment
+SELECT vendor_id,
+Product_id,
+sum(quantity * original_price) as WholePayment
+FROM farmers_market.vendor_inventory
+group by 1,2;
+
+Select *
+From #Temp_WholePayment
